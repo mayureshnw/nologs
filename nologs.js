dce874dab64b8cfac69f896a0b9c4c17b68d3e23
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-06-22 00:29:54 +0530

    remove logs code added

---
+++ nologs.js
@@ -0,0 +1,14 @@
+export default (fileInfo, api) => {
+  const j = api.jscodeshift;
+
+  return j(fileInfo.source)
+    .find(j.CallExpression, {
+        callee: {
+          type: 'MemberExpression',
+          object: { type: 'Identifier', name: 'console' },
+        },
+      }
+    )
+    .remove()
+    .toSource();
+};
