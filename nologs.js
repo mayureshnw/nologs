fddc6dcfad9239d69fcdb3547e397f6f395abfb6
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-06-24 19:20:53 +0530

    working well for single file

--- nologs.js
+++ nologs.js
@@ -4,63 +4,50 @@ const fs = require('fs');
 const program = require('commander');
 const exec = require('child_process').exec;
 
-const walk = (dir, option) => {
-    var results = []
-    var list = fs.readdirSync(dir)
-    list.forEach(function(file) {
-        var ext = file.split('.');
-        var isJs = ext[ext.length - 1] === "js";
-        if (isJs) {
-            file = dir + '/' + file
-            var stat = fs.statSync(file)
-            if (stat && stat.isDirectory()) results = results.concat(walk(file))
-            else results.push(file)
-        }
-    })
-    return results
-}
-
-const removeFromDir = (dir, option) => {
-    overwrite = (option) ? '' : '-d -p';
-    var fileList = walk(dir);
-    fileList.forEach((file) => {
-        exec(`jscodeshift -t transform.js ${file} ${overwrite}`, function(err, stdout, stderr) {
-          (err) ? console.error(stderr) : console.info(stdout);
-        });
+const removeFromDir = (dir, option, ignore) => {
+    const overwrite = (option) ? '' : '-d -p';
+    const ign = (ignore) ? '' : '--ignore-config .gitignore';
+    exec(`jscodeshift -t transform.js ${dir} ${overwrite} ${ignore}`, function(err, stdout, stderr) {
+        (err) ? console.error(stderr): console.info(stdout);
     });
 }
 
 
-const removeFromFile = (file, option) => {
-    overwrite = (option) ? '' : '-d -p';
-    exec(`jscodeshift -t transform.js ${file} ${overwrite}`, function(err, stdout, stderr) {
-      (err) ? console.error(stderr) : console.info(stdout);
+const removeFromFile = (file, option, ignore) => {
+    const overwrite = (option) ? '' : '-d -p';
+    const ign = (ignore) ? '' : '--ignore-config .gitignore';
+    exec(`jscodeshift -t transform.js ${file} ${overwrite} ${ignore}`, function(err, stdout, stderr) {
+        (err) ? console.error(stderr): console.info(stdout);
     });
 }
 
 program
     .command('file [path]')
     .option('-o, --overwrite', 'Original files will be overwritten with output')
+    .option('-i, --ignore', 'Ignore files from gitignore')
     .description('Give the directory to remove console.')
-    .action(function(dir, options) {
-    removeFromFile(file, options.overwrite)
-})
+    .action(function(path, options) {
+        removeFromFile(path, options.overwrite, options.ignore);
+    })
 
 
 program
     .command('dir [dir]')
     .option('-o, --overwrite', 'Original files will be overwritten with output')
+    .option('-i, --ignore', 'Ignore files from gitignore')
     .description('Give the directory to remove console.log')
-    .action(function(dir) {
-        removeFromDir(dir, options.overwrite);
+    .action(function(dir, options) {
+        removeFromDir(dir, options.overwrite, options.ignore);
     });
 
 program
     .command('here')
     .option('-o, --overwrite', 'Original files will be overwritten with output')
+    .option('-i, --ignore', 'Ignore files from gitignore')
     .description('takes the current directory to remove logs')
     .action(function(options) {
-        removeFromDir(__dirname, options.overwrite);
+        console.log(__dirname);
+        removeFromDir(__dirname, options.overwrite, options.ignore);
     });
 
 program
